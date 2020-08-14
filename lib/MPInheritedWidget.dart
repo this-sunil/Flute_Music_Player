64bf49c5db3ce2d196a46f54ce923e1aeb2710db
Sunil Shedge <51407211+surya88i@users.noreply.github.com>
2020-08-14 12:09:34 +0530

    Update MPInheritedWidget.dart

--- lib/MPInheritedWidget.dart
+++ lib/MPInheritedWidget.dart
@@ -1,13 +1,15 @@
+import 'package:flute_music_player/flute_music_player.dart';
 import 'package:flutter/material.dart';
 import 'song.dart';
 class MPInheritedWidget extends InheritedWidget{
   final SongData songData;
-  MPInheritedWidget(this.songData,child):super(child:child);
+  final Song song;
+  MPInheritedWidget(this.songData,this.song,child):super(child:child);
 
   static MPInheritedWidget of(BuildContext context){
     return context.dependOnInheritedWidgetOfExactType(aspect: MPInheritedWidget);
   }
   @override
-  bool updateShouldNotify(MPInheritedWidget oldWidget)=>songData!=oldWidget.songData;
+  bool updateShouldNotify(MPInheritedWidget oldWidget)=>songData!=oldWidget.songData && song!=oldWidget.song;
 
 }
