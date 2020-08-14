a22c4164043ef2807ad4318f2fda92d9c75bb738
Sunil Shedge <51407211+surya88i@users.noreply.github.com>
2020-08-14 12:08:30 +0530

    Update MusicPage.dart

--- lib/MusicPage.dart
+++ lib/MusicPage.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:cycles/MPInheritedWidget.dart';
 import 'package:cycles/song.dart';
 import 'package:flutter/material.dart';
@@ -23,24 +22,33 @@ class _MusicPageState extends State<MusicPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Music Player"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.notifications_none), onPressed: (){}),
+        ],
       ),
       body: ListView.builder(
+        scrollDirection: Axis.vertical,
+        physics: BouncingScrollPhysics(),
         itemCount:songData?.song?.length ?? 0,
         itemBuilder: (context,index){
           var s=songData.song[index];
-          return Card(
-              child: ListTile(
-              leading: s.albumArt==null?CircleAvatar(child: Icon(Icons.music_note)):ClipOval(child:Image.file(File.fromUri(Uri.parse(s.albumArt)),fit: BoxFit.cover,)),
-              title: Text(s.title),
-              subtitle: Text(s.artist),
-              trailing:Text('${Duration(milliseconds: s.duration).toString().split('.')[0]}'),
-              onTap: (){
-                songData.setCurrentIndex(index);
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>NowPlaying(songData,s)));
-              },
+          return Hero(
+                tag: s.title,
+                child: Card(
+                child: ListTile(
+                leading: s.albumArt==null?CircleAvatar(child: Icon(Icons.music_note)):ClipOval(child:Image.file(File.fromUri(Uri.parse(s.albumArt)),fit: BoxFit.cover,)),
+                title: Text(s.title),
+                subtitle: Text(s.artist),
+                trailing:Text('${Duration(milliseconds: s.duration).toString().split('.')[0]}'),
+                onTap: (){
+                  songData.setCurrentIndex(index);
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>NowPlaying(songData,s)));
+                },
+              ),
             ),
           );
         }),
+        
     );
   }
 }
