bfcf96004bd134e77bddd2e26d08fecdbed8fc9c
Sunil Shedge <51407211+surya88i@users.noreply.github.com>
2020-08-14 12:11:36 +0530

    Update song.dart

--- lib/song.dart
+++ lib/song.dart
@@ -1,38 +1,44 @@
-import 'dart:math';
 import 'package:flute_music_player/flute_music_player.dart';
-class SongData
-{
+import 'dart:math';
+
+class SongData {
   List<Song> song;
-  int currentSongIndex=-1;
+  int _currentSongIndex = -1;
   MusicFinder musicFinder;
-  SongData(this.song){
-    musicFinder=new MusicFinder();
+  SongData(this.song) {
+    musicFinder = new MusicFinder();
   }
-  List<Song> get songs=>song;
-  int get length=>song.length;
-  int get songNumber=>currentSongIndex+1;
-  setCurrentIndex(int index)
-  {
-    currentSongIndex=index;
+
+  List<Song> get songs => song;
+  int get length => song.length;
+  int get songNumber => _currentSongIndex + 1;
+
+  setCurrentIndex(int index) {
+    _currentSongIndex = index;
   }
+
+  int get currentIndex => _currentSongIndex;
+
   Song get nextSong {
-    if(currentSongIndex<length)
-    {
-      currentSongIndex++;
+    if (_currentSongIndex < length) {
+      _currentSongIndex++;
     }
-    if(currentSongIndex>=length) return null;
-    return song[currentSongIndex];
+    if (_currentSongIndex >= length) return null;
+    return song[_currentSongIndex];
   }
-  Song get randomSong{
-    Random r=new Random();
+
+  Song get randomSong {
+    Random r = new Random();
     return song[r.nextInt(song.length)];
   }
-  Song get prevSong{
-     if(currentSongIndex > 0)
-    {
-      currentSongIndex--;
+
+  Song get prevSong {
+    if (_currentSongIndex > 0) {
+      _currentSongIndex--;
     }
-    if(currentSongIndex < 0) return null;
-    return song[currentSongIndex];
+    if (_currentSongIndex < 0) return null;
+    return song[_currentSongIndex];
   }
+
+  MusicFinder get audioPlayer => musicFinder;
 }
