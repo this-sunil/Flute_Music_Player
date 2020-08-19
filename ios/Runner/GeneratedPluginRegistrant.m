6da0825391eb25f5d492ba12403591a1d7bfe616
Sunil Shedge <51407211+surya88i@users.noreply.github.com>
2020-08-19 15:34:36 +0530

    Add files via upload

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,28 +4,10 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<audioplayers/AudioplayersPlugin.h>)
-#import <audioplayers/AudioplayersPlugin.h>
-#else
-@import audioplayers;
-#endif
-
 #if __has_include(<flute_music_player/MusicFinderPlugin.h>)
 #import <flute_music_player/MusicFinderPlugin.h>
 #else
 @import flute_music_player;
-#endif
-
-#if __has_include(<flutter_media_notification/FlutterMediaNotificationPlugin.h>)
-#import <flutter_media_notification/FlutterMediaNotificationPlugin.h>
-#else
-@import flutter_media_notification;
-#endif
-
-#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
-#import <path_provider/FLTPathProviderPlugin.h>
-#else
-@import path_provider;
 #endif
 
 #if __has_include(<system_shortcuts/SystemShortcutsPlugin.h>)
@@ -37,10 +19,7 @@
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [AudioplayersPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioplayersPlugin"]];
   [MusicFinderPlugin registerWithRegistrar:[registry registrarForPlugin:@"MusicFinderPlugin"]];
-  [FlutterMediaNotificationPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterMediaNotificationPlugin"]];
-  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
   [SystemShortcutsPlugin registerWithRegistrar:[registry registrarForPlugin:@"SystemShortcutsPlugin"]];
 }
 
